c7832412f5e0f439a2c59bd837b99eb5a51d099b
Jovan Medford <jovanmedford@gmail.com>
2020-11-17 10:41:30 -0500

    Style Header Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,32 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAlignJustify, faSearch, faMoon } from '@fortawesome/free-solid-svg-icons'
+import Logo from '../../static/logopng.png'
 
 export default function Header() {
     return (
-        <div>
-            <i class="fas fa-align-justify"></i>
-        </div>
+        <div sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '80%',
+            margin: '0 auto',
+            height: '6rem',
+            }}>
+              <div className='Left'>
+              <FontAwesomeIcon icon={faAlignJustify}/>
+              </div>
+            <img src={Logo} alt={'Caribbean Scholar'} sx ={{
+              width: '9rem',
+              paddingBottom: '1.3rem'
+              }}/>
+              <div className='Right'>
+            <FontAwesomeIcon icon={faSearch} sx={{
+              marginRight: '0.5rem'
+            }}/>
+            <FontAwesomeIcon icon={faMoon}/>
+            </div> 
+          </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAlignJustify, faSearch, faMoon } from '@fortawesome/free-solid-svg-icons'
-import Logo from '../../static/logopng.png'
-export default function Home() {
-  return <div>
-    <FontAwesomeIcon icon={faAlignJustify}/>
-    <img src={Logo} sx ={{width: '10%'}}/>
-    <FontAwesomeIcon icon={faSearch}/>
-    <FontAwesomeIcon icon={faMoon}/>
-  </div>
+import Header from '../components/header'
+
+
+export default function BlogHome() {
+  return (
+    <Header />
+  )
 }

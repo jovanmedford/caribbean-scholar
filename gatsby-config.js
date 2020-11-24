5e919e71b04676ac0a9706bdef7e4d49b231fcec
Jovan Medford <jovanmedford@gmail.com>
2020-11-24 07:59:06 -0500

    Install gastby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    }
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-fontawesome-css',

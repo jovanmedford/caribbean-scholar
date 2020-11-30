a22f1e684ff38557b3989eefb8fea2be8bab7136
Jovan Medford <jovanmedford@gmail.com>
2020-11-30 14:23:17 -0500

    Add search filter credit: about Monica blog

--- src/pages/filter.js
+++ src/pages/filter.js
@@ -4,15 +4,44 @@ import Header from '../components/header'
 import { graphql, Link} from 'gatsby'
 import ListPostPreview from '../components/ListPostPreview'
 import TagsBar from '../components/tags-bar'
+import { useState } from 'react'
 
-export default function Filter({data}) {
-  console.log(data.allWpPost.nodes)
+export default function Filter(props) {
+  const { data } = props
+  const allPosts = data.allWpPost.nodes;
+  const [state, setState] = useState({
+    filteredPosts: [],
+    query: ''
+  })
+
+    const handleInputChange = (event) => {
+    const query = event.target.value 
+
+    const filteredPosts = allPosts.filter(post => {
+      const category = post.categories.nodes[0].name;
+      const title = post.title;
+      return (
+        title.toLowerCase().includes(query.toLowerCase()) ||
+        category.toLowerCase().includes(query.toLowerCase()) 
+        )
+      })
+
+      setState({
+        query,
+        filteredPosts})
+  }
+
+    const { filteredPosts, query} = state
+    const hasSearchResults = filteredPosts && query !== ""
+    const posts = hasSearchResults ? filteredPosts : allPosts 
+
+    
     return(
         <div>
           <Header />
           <TagsBar />
-          <input sx={{margin: '0 auto', display: 'block'}}></input>
-          <ListPostPreview info={data.allWpPost.nodes}/>
+          <input onChange={handleInputChange} sx={{margin: '0 auto', display: 'block'}}></input>
+          <ListPostPreview info={posts}/>
         </div>
     )
 }

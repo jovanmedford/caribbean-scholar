194374b1b2d9945a3f693b68eaa9bcca54eab59d
Jovan Medford <jovanmedford@gmail.com>
2020-12-04 15:36:01 -0500

    Add more styling to blog post

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -19,34 +19,47 @@ export default function BlogPost({data}) {
         <div>
             <Helmet>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous"/>
-<script type="module">
-    import renderMathInElement from "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.mjs";
-    renderMathInElement(document.body);
-</script>
+            <script type="module">
+            import renderMathInElement from "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.mjs";
+            renderMathInElement(document.body);
+            </script>
             </Helmet>
-
             <Header />
             <PostHeader category={post.categories.nodes[0].name} title={post.title} 
             authorImg={Me} authorName={post.author.node.firstName + ' ' + post.author.node.lastName} date={post.date}/>
             <Img fluid={source} sx={{
                 height: ['18rem','18rem','18rem'],
-                width: ['75%', '65%', '55%'],
+                width: ['75%', '65%', '60%'],
                 margin: '0 auto'
             }}
             
             />
             <article sx={{
-                width: ['75%', '65%', '55%'],
+                width: ['75%', '65%', '60%'],
                 margin: '2rem auto',
+                h2: {
+                    fontSize: ['1.8rem','2.4rem','2.8rem']
+                },
                 h3: {
-                    fontFamily: 'heading',
-                    fontSize: ['1.4rem','1.4rem','1.8rem'],
-                    marginBottom: '0.4rem',
+                    fontSize: ['1.5rem','2rem', '2.2rem'],
+                    marginBottom: '1rem',
                 },
                 p: {
-                    fontSize: ['1.2rem','1.5rem','1.4rem'],
+                    fontSize: ['1.2rem','1.4rem', '1.6rem'],
                     lineHeight: '1.6',
+                    marginBottom: '1rem',
                     marginTop: 0
+                },
+                blockQuote: {
+                    backgroundColor: 'muted',
+                    borderLeft: 'solid 3px',
+                    margin: '0 auto',
+                    borderColor: 'text',
+                    fontWeight: 'normal',
+
+                    p: {
+                        paddingLeft: '1rem'
+                    }
                 }
             }}
             dangerouslySetInnerHTML={{__html: post.content}}>

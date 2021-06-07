6c57c9d622f2b5c8f4ab6be398ce0f222ee449fd
Jovan Medford <jovanmedford@gmail.com>
2021-06-06 22:05:09 -0400

    Capitalize subject names in subjects object

--- src/utils/subjects.js
+++ src/utils/subjects.js
@@ -7,4 +7,12 @@ export const subjects = {
       "csec": new Date(2021,8,7,13),
       "cape": new Date(2021,4,4,9),
     },
+    "french": {
+      "csec": new Date(2021,2,1,13),
+      "cape": new Date(2021,2,5,9),
+    },
+    "English B": {
+      "csec": new Date(2021,7,10,13),
+      "cape": new Date(2021,6,24,9),
+    },
 }

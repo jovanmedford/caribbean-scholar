893293bfe5ecdd7cdd1c2e664311bfc2de23502f
Jovan Medford <jovanmedford@gmail.com>
2021-06-18 09:10:45 -0400

    Chore: Squash CountdownService into Countdown
    Error handling is no longer necessary in the Countdown component since we are checking for empty dates in the parent component.

--- src/components/exam-countdown/Countdown.js
+++ src/components/exam-countdown/Countdown.js
@@ -1,18 +1,23 @@
 /**@jsx jsx */
 import { jsx } from 'theme-ui'
+import { useState,useEffect } from 'react'
+import calculateCountdown from '../../utils/calculateCountdown'
+import CountdownUnit from './CountdownUnit'
 import remCalc from '../../utils/remCalc';
-import CountdownService from './CountdownService'
 
 export default function ({ isOpen, subjectDateTime }) {
-  let message;
-  const now = new Date();
- 
+  let [timeDifference, setTimeDifference] = useState(calculateCountdown(subjectDateTime))
 
-  if(subjectDateTime == null) {
-    message = <span>No exam</span>
-  } else if((subjectDateTime - now) < 0) {
-    message = <span>This exam has past.</span>
-  } 
+   //Decrement Countdown 
+   useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeDifference(calculateCountdown(subjectDateTime));
+    },1000);
+    return () => clearTimeout(timer);
+  });
+
+  //Deconstruct timeDifference
+  const [days,hours,minutes,seconds] = timeDifference;
 
   return (
     <section sx={{
@@ -24,7 +29,18 @@ export default function ({ isOpen, subjectDateTime }) {
       backgroundColor: '#28866C',
       display: isOpen ? 'block' : 'none',
     }}>
-      { message ? message : <CountdownService subjectDateTime={subjectDateTime}/> }
+      <div sx ={{
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        position: 'relative',
+        top: '50%',
+        marginTop: remCalc(-85),
+      }}>
+      <CountdownUnit number={days} unit="Days"/>
+      <CountdownUnit number={hours} unit="Hours"/>
+      <CountdownUnit number={minutes} unit="Minutes"/>
+      <CountdownUnit number={seconds} unit="Seconds"/>     
+      </div>
     </section>
   )
 }

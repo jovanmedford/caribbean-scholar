279741720126167fd46ff8b435b9823158a0d402
Jovan Medford <jovanmedford@gmail.com>
2021-08-03 10:29:36 -0400

    Chore: Remove broken gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,6 @@ module.exports = {
       }
     },
     'gatsby-plugin-react-helmet',
-    `gatsby-plugin-sitemap`,
-    'gatsby-plugin-robots-txt',
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-fontawesome-css',
   ],

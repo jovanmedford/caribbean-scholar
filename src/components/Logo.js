617a7cc9f7c222ad53b56b8fbd17ee3e25c92589
Jovan Medford <jovanmedford@gmail.com>
2021-10-09 12:05:13 -0400

    Refactor: Header component
    Cleanup header code by creating components for Logo and FontAwesomeIcon.
    Put style objects at the bottom of the page

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+export default function () {
+  return (
+    <StaticImage
+      src="../img/logopng.png"
+      alt="Caribbean Scholar Logo"
+      aspect-ratio={16 / 5}
+    />
+  )
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,79 +1,60 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faMoon } from '@fortawesome/free-solid-svg-icons'
-import Logo from '../img/logopng.png'
-import { useColorMode } from 'theme-ui'
-import { Link } from 'gatsby'
-import MenuService from './menuService'
+import { jsx } from "theme-ui"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch, faMoon } from "@fortawesome/free-solid-svg-icons"
+import { useColorMode } from "theme-ui"
+import { Link } from "gatsby"
+import Logo from "./Logo"
 
 export default function Header() {
   const [colorMode, setColorMode] = useColorMode()
-  setColorMode(colorMode === 'home' ? 'light' : colorMode)
-    return (
-        <header sx={{
-            width: ['85%','75%'],
-            margin: '3rem auto',
-            height: '2rem',
-            position: 'relative'
-            }}>
-            
-          <div className='Left' sx={{
-              position: 'absolute',
-              left: '0',
-              top: '50%',
-              width: '100%',
-               a: {
-                  textDecoration: 'none',
-                  color: 'text',
-                  padding: '0.2rem',
-                }
-            }}>
-              <MenuService type={1}/>
-              <Link to='/' sx={{
-                display:['none','inline-block'],
-                }}>
-                  Home
-              </Link>
-              <a href='https://school.thecaribbeanscholar.com/collections' sx={{
-                marginLeft: '0.75rem',
-                display:['none','inline-block']
-                }}>
-                  Courses
-                </a>
-          </div>
-          
-          <Link to='/blog'>
-            <img src={Logo} alt={'Caribbean Scholar Logo within link to blog home page'} sx ={{
-              width: '11em',
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              '-webkit-transform': 'translate(-50%, -50%)'
-              }}/>
-          </Link>
-              <div className='Right' sx={{
-                position: 'absolute',
-                right: '0',
-                top: '50%',
+  setColorMode(colorMode === "home" ? "light" : colorMode)
+  return (
+    <header sx={headerContainer}>
+      <div sx={linkContainer}>
+        <Link sx={nudge} to="/home">
+          Home
+        </Link>
+        <a href="https://school.thecaribbeanscholar.com/collections">Courses</a>
+      </div>
+      <div sx={logoContainer}>
+        <Logo />
+      </div>
+      <div sx={iconContainer}>
+        <FontAwesomeIcon sx={nudge} icon={faSearch} />
+        <FontAwesomeIcon sx={nudge} icon={faMoon} />
+      </div>
+    </header>
+  )
+}
 
-                svg: {
-                  ':hover':{
-                    cursor: 'pointer'
-                }
-              }
-              }}>
-                <Link to='/filter' sx={{color: 'text'}}>
-                    <FontAwesomeIcon icon={faSearch} alt='redirects to blog search page' sx={{
-                    marginRight: '0.75rem'
-                  }}/>
-                </Link>
-                <FontAwesomeIcon alt='toggles theme to dark mode' icon={faMoon}
-                  onClick={e => {
-                    setColorMode(colorMode === 'light' ? 'dark' : 'light')
-                  }}
-                />
-            </div> 
-          </header>
-    )
+const flexItemWidth = "33.33%"
+
+const headerContainer = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+}
+
+const logoContainer = {
+  width: "10rem",
+}
+
+const linkContainer = {
+  a: {
+    color: "text",
+  },
+  marginTop: "1rem",
+  width: flexItemWidth,
+}
+
+const iconContainer = {
+  marginTop: "1rem",
+  display: "flex",
+  justifyContent: "right",
+  width: flexItemWidth,
+}
+
+const nudge = {
+  marginRight: "1rem",
 }
